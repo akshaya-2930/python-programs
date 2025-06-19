b4f96eac339a7ac5526796fd72147f706cb7478e
akshaya-2930 <akkiakshaya142@gmail.com>
2025-06-19 14:13:00 +0530

    Create 31 array .py

---
+++ 31 array .py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Sample array
+data = np.array([12, 15, 7, 10, 18, 21, 13])
+
+# Calculations
+mean = np.mean(data)
+median = np.median(data)
+std_dev = np.std(data)
+
+# Results
+print("Array:", data)
+print("Mean:", mean)
+print("Median:", median)
+print("Standard Deviation:", std_dev)
+Array: [12 15  7 10 18 21 13]  
+Mean: 13.714285714285714  
+Median: 13.0  
+Standard Deviation: 4.295346318982906
